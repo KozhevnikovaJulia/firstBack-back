0b48256e175c8c0c538985428735dda3117bb300
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-04-02 20:59:36 +0300

    start

--- withMongo/index.js
+++ withMongo/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 7777;
+const port = process.env.PORT || 7777;
 const usersRouter = require('./usersRouter');
 const tasksRouter = require('./tasksRouter');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true});
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true});
 
-//mongoose
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-});
+// //mongoose
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+// });
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use((req,res) => {
   res.send(404)
