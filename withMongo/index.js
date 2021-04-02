e7f11f7f99f004b8504e4093abdc544018724a16
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-04-02 21:32:10 +0300

    addad mongo

--- withMongo/index.js
+++ withMongo/index.js
@@ -5,19 +5,19 @@ const usersRouter = require('./usersRouter');
 const tasksRouter = require('./tasksRouter');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true, useUnifiedTopology: true});
 
-// //mongoose
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-// });
+//mongoose
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use((req,res) => {
   res.send(404)
